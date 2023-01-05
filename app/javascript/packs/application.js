eb4ba36863eb8e9117544443b7e5e009abffcb16
Brett Seifried <seifriedbrett@gmail.com>
2023-01-05 11:42:25 -0800

    webpack error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs").start();
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("turbolinks").start();
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/activestorage").start();
+
+require("channels");
+
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "stylesheets/application";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
